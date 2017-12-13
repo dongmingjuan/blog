b4375c0d352f2fc469c147098fcf72db322bab82
dongmingjuan <https://github.com/dongmingjuan>
2017-12-13 10:24:10 +0800

    add post controller & model

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,8 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :posts
   end
-  namespace :admin do
-    resources :groups
-  end
-  resources :groups
-  root "groups#index"
+  resources :posts
+  root "posts#index"
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
