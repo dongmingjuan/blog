64222760760d6f4680a0ed1819e6b397da4b7a6b
dongmingjuan <https://github.com/dongmingjuan>
2017-12-13 12:09:15 +0800

    finish the blog that common user can see

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,23 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!
   load_and_authorize_resource
+  before_action :set_post, only: [:show]
   layout "admin"
+  def index
+    @posts = Post.all
+    @posts = @posts.page params[:page]
+  end
+
+  def show
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :posts
   end
-  resources :posts
+  resources :posts do
+    resources :comments
+  end
   root "posts#index"
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
